59401fcf850a2ae10fc5ac62b81ceb7bab043fae
gerardojgarcia <gerardojgarcia@pm.me>
2023-01-15 15:40:21 -0800

    updated vue-gtag plugin for Google Analytics Tracking.

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -1,11 +1,11 @@
-import VueGtag from 'vue-gtag-next'
+import VueGtag from 'vue-gtag'
 
 import {defineNuxtPlugin} from "nuxt/app";
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component(VueGtag, {
+    nuxtApp.vueApp.use(VueGtag, {
         property: {
             id: 'G-2N50LHTJC8'
         }
-    })
+    }, nuxtApp.$router)
 })
