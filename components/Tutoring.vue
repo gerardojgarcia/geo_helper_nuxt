479064f1e45b1ccd9738059a8d66923e44f3fc1c
Gerardo Garcia <gerardojgarcia@pm.me>
2023-12-18 03:21:57 -0800

    updated tutoring image

--- components/Tutoring.vue
+++ components/Tutoring.vue
@@ -6,8 +6,7 @@ export default {
 
 <template>
   <div
-    class="tutor-container w-full flex flex-col md:flex-row md:py-16 pb-4 relative justify-between px-4 md:px-24"
-  >
+    class="tutor-container w-full flex flex-col md:flex-row md:py-16 pb-4 relative items-center justify-between px-4 md:px-24">
     <div class="tutor-info text-left flex flex-col md:m-16 pt-8">
       <div class="mt-8 mb-16" data-aos="fade-up">
         <h3 class="text-5xl ml-8 mb-2 text-shadow-lg">Get Online Help Right Now!</h3>
@@ -16,43 +15,18 @@ export default {
       </div>
 
       <div class="tutor-form md:ml-2 w-full" data-aos="fade-up-right">
-        <form action="submit" class="flex flex-col w-full m-auto space-y-6">
+        <form action="submit" class="flex flex-col  m-auto space-y-6">
           <div class="flex items-center flex-col space-y-4">
-            <input
-              class="shadow-md rounded-lg p-2"
-              type="text"
-              name="first-name"
-              placeholder="First Name"
-              required
-            />
-            <input
-              class="shadow-md rounded-lg p-2"
-              type="text"
-              name="last-name"
-              placeholder="Last Name"
-              required
-            />
+            <input class="shadow-md rounded-lg p-2" type="text" name="first-name" placeholder="First Name" required />
+            <input class="shadow-md rounded-lg p-2" type="text" name="last-name" placeholder="Last Name" required />
           </div>
           <div class="flex items-center flex-col space-y-4">
-            <input
-              class="shadow-md rounded-lg p-2"
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-            />
-            <textarea
-              class="shadow-md rounded-lg p-2"
-              type="text"
-              name="name"
-              placeholder="Question"
-              required
-            ></textarea>
+            <input class="shadow-md rounded-lg p-2" type="email" name="email" placeholder="Email" required />
+            <textarea class="shadow-md rounded-lg p-2" type="text" name="name" placeholder="Question" required></textarea>
           </div>
 
           <button
-            class="self-center text-white rounded-sm bg-teal-500 shadow-md px-4 py-2 hover:bg-teal-400 border-0 hover:shadow-xl hover:text-white w-32 my-8"
-          >
+            class="self-center text-white rounded-sm bg-teal-500 shadow-md px-4 py-2 hover:bg-teal-400 border-0 hover:shadow-xl hover:text-white w-32 my-8">
             Submit
           </button>
         </form>
@@ -60,7 +34,7 @@ export default {
     </div>
 
     <div class="tutor-img hidden md:inline mt-2 mr-16 md:ml-8" data-aos="fade-up-right">
-      <img src="/imgs/tutoring.svg" alt="" class="w-full md:3/4" />
+      <img src="/geologist_research_1.png" alt="" class="w-full md:3/4" />
     </div>
   </div>
 </template>
@@ -73,6 +47,17 @@ export default {
 .tutor-img {
   width: 50vh;
   height: 50vh;
+}
+
+form {
+  width: 15vw;
+  padding-top: 2rem;
+}
+
+input,
+textarea {
+  width: 25vw;
+
 }
 
 @media (max-width: 800px) {
